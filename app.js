e9143be87dddc4c0c7008533ba351c438d72d9a7
strohy120 <sarah.stroh@flatironschool.com>
2015-01-27 15:01:34 -0500

    initialize gitrepo

---
+++ app.js
@@ -0,0 +1,85 @@
+var App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function(){
+  this.route('about', { path: '/aboutus'} );
+  this.resource('countries', function(){
+    this.resource('country', {path: '/:name'});
+  });
+});
+
+App.IndexController = Ember.Controller.extend({
+  countriesCount: function() {
+    return App.COUNTRIES.length
+  }.property(),
+  // map: '/images/logo.png',
+  time: function() {
+    return (new Date()).toDateString()
+  }.property()
+});
+
+App.COUNTRIES = [
+{
+  name: 'Cambodia',
+  continent: 'South East Asia',
+  date: 'January 2014 and May 2014'
+  // date:
+
+},
+{
+  name: 'Thailand',
+  continent: 'South East Asia',
+  date: 'December 2013, February 2014, April 2014, July 2014'
+},
+
+{
+  name: 'Vietnam',
+  continent: 'South East Asia',
+  date: 'June 2014'
+},
+
+{
+  name: 'Myanmar',
+  continent: 'South East Asia',
+  date: 'August 2014'
+},
+
+{
+  name: 'Lao',
+  continent: 'South East Asia',
+  date: 'July 2014'
+},
+
+{
+  name: 'Singapore',
+  continent: 'South East Asia',
+  date: 'September 2013 to June 2014'
+},
+
+{
+  name: 'Malaysia',
+  continent: 'South East Asia',
+  date: 'November 2013 and May 2014'
+},
+
+{
+  name: 'Indonesia',
+  continent: 'South East Asia',
+  date: 'August 2014'
+}
+
+];
+
+App.CountriesRoute = Ember.Route.extend({
+  model: function() {
+    return App.COUNTRIES; //can be object or an array
+  }
+
+});
+
+AppCountryRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.PRODUCTS.findBy('name', params.name); //go to '/cmabodia', it will find that name and find the object
+  }
+});

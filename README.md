0920eb46243a814f14bc97740335db7ea7a81423
strohy120 <sarah.stroh@flatironschool.com>
2015-01-28 17:59:39 -0500

    push

---
+++ README.md
@@ -0,0 +1,10 @@
+EMber DAta
+Default adapter for HTTP server using JSON
+
+App.ApplicationAdapter = DS.RESTAdapter.extend();
+
+can do fixture adapter fr hardcoded
+
+all models must have unique id
+
+ember had store for all records, used to send and retrive records
--- app.js
+++ app.js
@@ -13,7 +13,6 @@ App.IndexController = Ember.Controller.extend({
   countriesCount: function() {
     return App.COUNTRIES.length
   }.property(),
-  // map: '/images/logo.png',
   time: function() {
     return (new Date()).toDateString()
   }.property()
@@ -23,61 +22,70 @@ App.COUNTRIES = [
 {
   name: 'Cambodia',
   continent: 'South East Asia',
-  date: 'January 2014 and May 2014'
-  // date:
+  date: 'January 2014 and May 2014',
+  cities: 'Siem Reap, Koh Rong, Phnom Penh, Sihanoukville, Kampot'
 
 },
 {
   name: 'Thailand',
   continent: 'South East Asia',
-  date: 'December 2013, February 2014, April 2014, July 2014'
+  date: 'December 2013, February 2014, April 2014, July 2014',
+  cities: 'Bangkok, Koh Tao, Koh Lanta, Koh Phi Phi, Pai, Chiang Mai, Koh Phagnan'
 },
 
 {
   name: 'Vietnam',
   continent: 'South East Asia',
-  date: 'June 2014'
+  date: 'June 2014',
+  cities: 'Saigon (Ho Chi Minh City, Mui Ne, Nha Trang, Hoi An, Hanoi, Halong Bay, Sapa'
 },
 
 {
   name: 'Myanmar',
   continent: 'South East Asia',
-  date: 'August 2014'
+  date: 'August 2014',
+  cities: 'Yangon, Hsi Paw, Mandalay, Inle Lake, Bagan, Kalaw'
 },
 
 {
   name: 'Lao',
   continent: 'South East Asia',
-  date: 'July 2014'
+  date: 'July 2014',
+  cities: 'Luang Prabang, Vang Vieng'
 },
 
 {
   name: 'Singapore',
   continent: 'South East Asia',
-  date: 'September 2013 to June 2014'
+  date: 'September 2013 to June 2014',
+  cities: 'Singapore'
 },
 
 {
   name: 'Malaysia',
   continent: 'South East Asia',
-  date: 'November 2013 and May 2014'
+  date: 'November 2013 and May 2014',
+  cities: 'Kuala Lumpur, Penang'
 },
 
 {
   name: 'Indonesia',
   continent: 'South East Asia',
-  date: 'August 2014'
+  date: 'August 2014',
+  cities: 'Gili Islands, Seminyak and Ubud, Bali, Jyojyakarta'
 },
 
 {
   name: 'Japan',
   continent: 'South East Asia',
-  date: 'June 2014'
+  date: 'June 2014',
+  cities: 'Tokyo, Kyoto'
 },
 {
   name: 'Australia',
   continent: 'South East Asia',
-  date: 'New Year 2014-2015'
+  date: 'New Year 2014-2015',
+  cities: 'Sydney'
 }
 
 ];
@@ -96,5 +104,10 @@ AppCountryRoute = Ember.Route.extend({
 });
 
 App.Product = DS.Model.extend({
+  name: DS.attr('string'),
+  continent: DS.attr('string'), <!-- //will be implied if not specified -->
+  date: DS.attr('string'),
+  cities: DS.attr('string')
+});
 
-})
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
